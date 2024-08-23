f19b2ee89c21194e7d2b2257097398a45bcb894c
Kellinton <kellintonbrasil@gmail.com>
2024-08-23 15:03:23 -0300

    style: estilizado a página

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,15 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      gridTemplateRows: {
+        'layout': '1fr auto',
+      },
       fontFamily: {
         'spartan': ['League Spartan', 'sans-serif'],
       },
       colors: {
         'azul': ['hsl(220, 100%, 50%)'],
-        'azul-claro': ['hsl(200, 80%, 67%)'],
+        'azul-claro': ['hsl(220, 100%, 60%)'],
         'cinza': ['hsl(303, 10%, 53%)'],
         'branco': ['hsl(0, 0%, 100%)'],  
         'branco-neve': ['hsl(300, 24%, 96%)'],
