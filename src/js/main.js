643239f087808b7dc4598478ca814a1be154ef97
Kellinton <kellintonbrasil@gmail.com>
2024-08-24 11:11:12 -0300

    feat: adicionar textos em hexadecimal que fazem referência as cores geradas

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 const btn = document.getElementById('gerar');
 const coresDiv = [...document.getElementsByClassName('cor')];
+const textosDiv = [...document.getElementsByClassName('texto')];
 
 
 btn.addEventListener('click', () => {
-
+    let contador = 0;
     coresDiv.forEach(cor => {
 
         const caracteres = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
@@ -12,13 +13,14 @@ btn.addEventListener('click', () => {
         for (let i = 0; i < 6; i++) {
             const numeroSorteado = Math.floor(Math.random() * 16);
     
-        corGerada.push(caracteres[numeroSorteado]);
+            corGerada.push(caracteres[numeroSorteado]);
         }
-    
-        cor.style.backgroundColor = `#${corGerada.join('')}`;
-    
-    });
-    
 
+        textosDiv[contador].innerHTML = `#${corGerada.join('')}`;
+        cor.style.backgroundColor = `#${corGerada.join('')}`;
+
+        contador++;
+
+    });
 
 })
